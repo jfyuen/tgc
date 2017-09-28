2510cdd1fa60bde7b9c1f95f5222269126e74363
Jean-François YUEN <jfyuen@gmail.com>
2017-09-28 23:49:35 +0200

    Test In/OutNode together

--- pipe_test.go
+++ pipe_test.go
@@ -6,6 +6,8 @@ import (
 	"time"
 )
 
+// TODO: Sleeping/Waiting for tests is ugly, find a nice way to Read/Write at the good time
+
 func TestPipeReceive(t *testing.T) {
 	fromCh := make(chan Message)
 	toCh := make(chan Message)
@@ -14,8 +16,8 @@ func TestPipeReceive(t *testing.T) {
 
 	var b bytes.Buffer
 	go p.receive(&b, errCh)
-	hello := "hello"
-	_, err := b.Write([]byte(hello))
+	hello := []byte("hello")
+	_, err := b.Write(hello)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -24,10 +26,9 @@ func TestPipeReceive(t *testing.T) {
 	timer := time.NewTimer(delay)
 
 	select {
-	case e := <-toCh:
-		s := string(e.payload)
-		if s != hello {
-			t.Errorf("received wrong data: %v vs %v", hello, s)
+	case msg := <-toCh:
+		if !bytes.Equal(msg.payload, hello) {
+			t.Errorf("received wrong data: %v vs %v", hello, msg.payload)
 		}
 	case <-timer.C:
 		t.Errorf("data not received after %v", delay)
@@ -39,20 +40,42 @@ func TestPipeReceive(t *testing.T) {
 func TestPipeSend(t *testing.T) {
 	fromCh := make(chan Message)
 	toCh := make(chan Message)
-	errCh := make(chan error)
+	errCh := make(chan error, 1)
 	p := OutNode{from: fromCh, to: toCh, err: make(chan error), addr: "test_conn"}
 
-	hello := "hello"
 	b := bytes.Buffer{}
 	go p.send(&b, errCh)
-	fromCh <- NewMessage([]byte(hello))
-	time.Sleep(time.Millisecond * 100)
+	msg := NewMessage([]byte("hello"))
+	msg.header.eof = true
+	fromCh <- msg
+	<-p.err
 	received := b.Bytes()
 	if len(received) == 0 {
 		t.Fatal("no data received")
 	}
-	s := string(received)
-	if s != hello {
-		t.Errorf("received wrong data: %v vs %v", hello, s)
+	if !bytes.Equal(received, msg.payload) {
+		t.Errorf("received wrong data: %v vs %v", msg.payload, received)
+	}
+}
+
+func TestInOutNode(t *testing.T) {
+	fromCh := make(chan Message)
+	toCh := make(chan Message)
+	outErrCh := make(chan error)
+	pOut := OutNode{from: fromCh, to: toCh, err: make(chan error), addr: "out_conn"}
+	outRW := bytes.Buffer{}
+	go pOut.send(&outRW, outErrCh)
+
+	inErrCh := make(chan error)
+	pIn := InNode{from: toCh, to: fromCh, err: make(chan error), addr: "in_conn"}
+	inRW := bytes.Buffer{}
+	msg := NewMessage([]byte("hello"))
+	msg.header.eof = true
+	msg.WriteTo(&inRW)
+	pIn.Wait(&inRW, inErrCh)
+	time.Sleep(time.Millisecond * 100)
+	b := outRW.Bytes()
+	if !bytes.Equal(b, msg.payload) {
+		t.Errorf("sent message %v and received message %v are different", msg.payload, b)
 	}
 }
